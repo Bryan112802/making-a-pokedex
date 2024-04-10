17a2ba16030c7530c3f33e4d37b3b03645705f0e
Bryan112802 <sorianob@mail.gvsu.edu>
2024-04-10 18:18:26 -0400

    Final

--- src/index.js
+++ src/index.js
@@ -4,21 +4,25 @@ const axios = require('axios')
 const rl = readline.createInterface(process.stdin, process.stdout)
 
 function showMenu () {
+  console.log('----------------------------')
   console.log('Pokedex')
   console.log('1. Search for a Pokemon')
   console.log('2. Search for an Item')
   console.log('3. Search for a Move')
+  console.log('4. Exit')
+  console.log('----------------------------')
+
+  console.log('Choose a Prompt!')
 }
 
-// TODO: Fix after "Enter pokemon" and "Your Input"
-
-// Update to actually use term
 function searchPoke (term) {
-  axios.get('https://pokeapi.co/api/v2/pokemon/pikachu')
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${term}`)
     .then(response => {
-      console.log(`ID: ${response.data.id}`)
+      printPoke(response.data)
     })
-    .catch()
+    .catch(error => {
+      console.log(error.message)
+    })
 }
 
 function prompt (cb) {
@@ -27,40 +31,75 @@ function prompt (cb) {
   })
 }
 
-/*
 function printPoke (json) {
-
+  console.log('----------------------------')
+  console.log(`Pokemon Name: ${json.name}`)
+  console.log(`Weight: ${json.weight}`)
+  console.log(`Height: ${json.height}`)
+  console.log(`Base Experience: ${json.base_experience}`)
+  console.log('Moves: ')
+  for (let i = 0; i < json.moves.length; i++) {
+    console.log(`${i + 1}. ${json.moves[i].move.name}`)
+  }
+  console.log('----------------------------')
+  rl.question('Press enter to return to menu', () => run())
 }
 
 function searchItem (term) {
-
+  axios.get(`https://pokeapi.co/api/v2/item/${term}`)
+    .then(response => {
+      printItem(response.data)
+    })
+    .catch(error => {
+      console.log(error.message)
+    })
 }
 
 function printItem (json) {
-
+  console.log('----------------------------')
+  console.log(`Item Name: ${json.name}`)
+  console.log(`ID: ${json.id}`)
+  console.log(`Cost: ${json.cost}`)
+  console.log(`Category: ${json.category.name}`)
+  console.log('----------------------------')
+  rl.question('Press enter to return to menu', () => run())
 }
 
 function searchMove (term) {
-
+  axios.get(`https://pokeapi.co/api/v2/move/${term}`)
+    .then(response => {
+      printMove(response.data)
+    })
+    .catch(error => {
+      console.log(error.message)
+    })
 }
 
 function printMove (json) {
-
+  console.log('----------------------------')
+  console.log(`Move Name: ${json.name}`)
+  console.log(`ID: ${json.id}`)
+  console.log(`Power: ${json.power}`)
+  console.log(`Accuracy: ${json.accuracy}`)
+  console.log('----------------------------')
+  rl.question('Press enter to return to menu', () => run())
 }
-*/
+
 function run () {
   showMenu()
-  rl.question('Choose a prompt number\n', (response) => {
+  prompt((response) => {
     if (response.trim() === '1') {
-      rl.question('Enter pokemon term', (pokeTerm) => {
-        prompt(searchPoke)
-      })
+      console.log('Enter Pokemon Name or ID:')
+      prompt((term) => searchPoke(term))
     } else if (response.trim() === '2') {
-      console.log('2')
-      // searchItem('item')
+      console.log('Search for Item Name or ID:')
+      prompt((json) => searchItem(json))
     } else if (response.trim() === '3') {
-      console.log('3')
-      // searchMove('move')
+      console.log('Search for Move Name or ID:')
+      prompt((term) => searchMove(term))
+    } else if (response.trim() === '4') {
+      console.log('Exiting')
+      rl.close()
     } else {
       console.log('Invalid input, try again.')
       run()
